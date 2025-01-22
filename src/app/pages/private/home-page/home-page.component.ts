f9aa03cb7a2c0ba96e17a77b876b605d54a8e9fb
Yocmar Rafael Jiménez González <yocmarjigon@gmail.com>
2025-01-22 14:57:48 -0500

    Avances en el home-page

--- src/app/pages/private/home-page/home-page.component.ts
+++ src/app/pages/private/home-page/home-page.component.ts
@@ -1,13 +1,88 @@
-import { Component } from '@angular/core';
-import { ImgTopComponent } from "../../../components/img-top/img-top.component";
+import { Component, OnInit } from '@angular/core';
+import { ImgTopComponent } from '../../../components/img-top/img-top.component';
+import { ProductCardComponent } from '../../../components/card/product-card/product-card.component';
+import { Product } from 'src/app/models/Product';
+import { ButtonModule } from 'primeng/button';
+import { CarouselModule } from 'primeng/carousel';
+import { TagModule } from 'primeng/tag';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [ImgTopComponent],
+  imports: [
+    ImgTopComponent,
+    ProductCardComponent,
+    NgFor,
+    CarouselModule,
+    ButtonModule,
+    TagModule,
+  ],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrl: './home-page.component.scss',
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  products: Product[] = [];
+  responsiveOptions = [
+    {
+      breakpoint: '800px',
+      numVisible: 3,
+      numScroll: 3,
+    },
+    {
+      breakpoint: '600px',
+      numVisible: 2,
+      numScroll: 2,
+    },
+    {
+      breakpoint: '300px',
+      numVisible: 1,
+      numScroll: 1,
+    },
+  ];
 
+  ngOnInit(): void {
+    this.products = [
+      {
+        title: 'Arepa de huevo',
+        img: '/assets/images/arepa-de-huevo.jpeg',
+        price: 2000,
+      },
+      {
+        title: 'Arepa de huevo',
+        img: '/assets/images/arepa-de-huevo.jpeg',
+        price: 2000,
+      },
+      {
+        title: 'Arepa de huevo',
+        img: '/assets/images/arepa-de-huevo.jpeg',
+        price: 2000,
+      },
+      {
+        title: 'Arepa de huevo',
+        img: '/assets/images/arepa-de-huevo.jpeg',
+        price: 2000,
+      },
+      {
+        title: 'Arepa de huevo',
+        img: '/assets/images/arepa-de-huevo.jpeg',
+        price: 2000,
+      },
+      {
+        title: 'Arepa de huevo',
+        img: '/assets/images/arepa-de-huevo.jpeg',
+        price: 2000,
+      },
+      {
+        title: 'Arepa de huevo',
+        img: '/assets/images/arepa-de-huevo.jpeg',
+        price: 2000,
+      },
+      {
+        title: 'Arepa de huevo',
+        img: '/assets/images/arepa-de-huevo.jpeg',
+        price: 2000,
+      },
+    ];
+  }
 }
